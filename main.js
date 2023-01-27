fb00ea657f8ade7e02a03472ae9eb1a633eac7e3
aziz srarfi <azizsrarfi79@gmail.com>
2023-01-27 23:07:54 +0100

    hidden btns

--- main.js
+++ main.js
@@ -18,5 +18,14 @@ var redButtons = document.getElementsByClassName("btn-red");
 for (let i = 0; i < 4; i++) {
   redButtons[i].addEventListener("mouseover", () => {
     redButtons[i].className = "btn btn-red btn-red-move";
+    if (i == 0) {
+      document.getElementById("green-btn").className = "btn hidden-btn-show";
+    } else if (i == 1) {
+      document.getElementById("yellow-btn").className = "btn hidden-btn-show";
+    } else if (i == 2) {
+      document.getElementById("blue-btn").className = "btn hidden-btn-show";
+    } else {
+      document.getElementById("purple-btn").className = "btn hidden-btn-show";
+    }
   });
 }
